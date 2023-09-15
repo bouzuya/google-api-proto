7e9d3b6a23554ab04ce11b918e0037bb995cb6c8
mechiru <mechiru@users.noreply.github.com>
2023-09-15 00:45:21 +0000

    google-api-proto: regenerate code

--- google-api-proto/src/google/cloud/dataproc/v1/mod.rs
+++ google-api-proto/src/google/cloud/dataproc/v1/mod.rs
@@ -1387,22 +1387,20 @@ pub struct InstanceGroupConfig {
     /// Platform](<https://cloud.google.com/dataproc/docs/concepts/compute/dataproc-min-cpu>).
     #[prost(string, tag = "9")]
     pub min_cpu_platform: ::prost::alloc::string::String,
-    /// Optional. The minimum number of instances to create.
-    /// If min_num_instances is set, min_num_instances is used for a criteria to
-    /// decide the cluster. Cluster creation will be failed by being an error state
-    /// if the total number of instances created is less than the
-    /// min_num_instances.
-    /// For example, given that num_instances = 5 and min_num_instances = 3,
-    /// * if 4 instances are created and then registered successfully but one
-    /// instance is failed, the failed VM will be deleted and the cluster will be
-    /// resized to 4 instances in running state.
-    /// * if 2 instances are created successfully and 3 instances are failed,
-    /// the cluster will be in an error state and does not delete failed VMs for
-    /// debugging.
-    /// * if 2 instance are created and then registered successfully but 3
-    /// instances are failed to initialize, the cluster will be in an error state
-    /// and does not delete failed VMs for debugging.
-    /// NB: This can only be set for primary workers now.
+    /// Optional. The minimum number of primary worker instances to create.
+    /// If `min_num_instances` is set, cluster creation will succeed if
+    /// the number of primary workers created is at least equal to the
+    /// `min_num_instances` number.
+    ///
+    /// Example: Cluster creation request with `num_instances` = `5` and
+    /// `min_num_instances` = `3`:
+    ///
+    /// *  If 4 VMs are created and 1 instance fails,
+    ///     the failed VM is deleted. The cluster is
+    ///     resized to 4 instances and placed in a `RUNNING` state.
+    /// *  If 2 instances are created and 3 instances fail,
+    ///     the cluster in placed in an `ERROR` state. The failed VMs
+    ///     are not deleted.
     #[prost(int32, tag = "12")]
     pub min_num_instances: i32,
     /// Optional. Instance flexibility Policy allowing a mixture of VM shapes and
@@ -1664,7 +1662,7 @@ pub struct NodeGroup {
 }
 /// Nested message and enum types in `NodeGroup`.
 pub mod node_group {
-    /// Node group roles.
+    /// Node pool roles.
     #[derive(
         Clone,
         Copy,
@@ -1680,7 +1678,7 @@ pub mod node_group {
     pub enum Role {
         /// Required unspecified role.
         Unspecified = 0,
-        /// Job drivers run on the node group.
+        /// Job drivers run on the node pool.
         Driver = 1,
     }
     impl Role {
@@ -2513,6 +2511,25 @@ pub struct DiagnoseClusterRequest {
     /// Required. The cluster name.
     #[prost(string, tag = "2")]
     pub cluster_name: ::prost::alloc::string::String,
+    /// Optional. The output Cloud Storage directory for the diagnostic
+    /// tarball. If not specified, a task-specific directory in the cluster's
+    /// staging bucket will be used.
+    #[prost(string, tag = "4")]
+    pub tarball_gcs_dir: ::prost::alloc::string::String,
+    /// Optional. Time interval in which diagnosis should be carried out on the
+    /// cluster.
+    #[prost(message, optional, tag = "6")]
+    pub diagnosis_interval: ::core::option::Option<
+        super::super::super::r#type::Interval,
+    >,
+    /// Optional. Specifies a list of jobs on which diagnosis is to be performed.
+    /// Format: projects/{project}/regions/{region}/jobs/{job}
+    #[prost(string, repeated, tag = "10")]
+    pub jobs: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
+    /// Optional. Specifies a list of yarn applications on which diagnosis is to be
+    /// performed.
+    #[prost(string, repeated, tag = "11")]
+    pub yarn_application_ids: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
 }
 /// The location of diagnostic output.
 #[allow(clippy::derive_partial_eq_without_eq)]
