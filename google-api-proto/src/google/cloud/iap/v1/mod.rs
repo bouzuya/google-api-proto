ce7dda7e7cb6145ebb6122863658cbd3ff532521
mechiru <mechiru@users.noreply.github.com>
2023-09-16 00:44:24 +0000

    google-api-proto: regenerate code

--- google-api-proto/src/google/cloud/iap/v1/mod.rs
+++ google-api-proto/src/google/cloud/iap/v1/mod.rs
@@ -206,6 +206,9 @@ pub struct OAuthSettings {
     /// since access behavior is managed by IAM policies.
     #[prost(message, optional, tag = "2")]
     pub login_hint: ::core::option::Option<::prost::alloc::string::String>,
+    /// List of Oauth client IDs allowed to programmatically authenticate with IAP.
+    #[prost(string, repeated, tag = "5")]
+    pub programmatic_clients: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
 }
 /// Configuration for IAP reauthentication policies.
 #[allow(clippy::derive_partial_eq_without_eq)]
