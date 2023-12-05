a083090b2ea8fc392fabecedf3e3739e1f8609f4
mechiru <mechiru@users.noreply.github.com>
2023-12-05 00:44:56 +0000

    deploy: f441197a2727ade91ee0e4acd086c3f2820e2877

--- google_api_proto/google/firestore/admin/v1/database/sidebar-items.js
+++ google_api_proto/google/firestore/admin/v1/database/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["AppEngineIntegrationMode","ConcurrencyMode","DatabaseType"]};
+window.SIDEBAR_ITEMS = {"enum":["AppEngineIntegrationMode","ConcurrencyMode","DatabaseType","PointInTimeRecoveryEnablement"]};
