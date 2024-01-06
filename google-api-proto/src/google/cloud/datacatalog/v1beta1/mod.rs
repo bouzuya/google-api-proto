a2a8504b5c639b7b372ad1c72ba80d09ab4aa27b
mechiru <mechiru@users.noreply.github.com>
2024-01-06 00:37:38 +0000

    google-api-proto: regenerate code

--- google-api-proto/src/google/cloud/datacatalog/v1beta1/mod.rs
+++ google-api-proto/src/google/cloud/datacatalog/v1beta1/mod.rs
@@ -194,7 +194,7 @@ pub struct SystemTimestamps {
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Taxonomy {
-    /// Output only. Resource name of this taxonomy, whose format is:
+    /// Identifier. Resource name of this taxonomy, whose format is:
     /// "projects/{project_number}/locations/{location_id}/taxonomies/{id}".
     #[prost(string, tag = "1")]
     pub name: ::prost::alloc::string::String,
@@ -289,7 +289,7 @@ pub mod taxonomy {
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct PolicyTag {
-    /// Output only. Resource name of this policy tag, whose format is:
+    /// Identifier. Resource name of this policy tag, whose format is:
     /// "projects/{project_number}/locations/{location_id}/taxonomies/{taxonomy_id}/policyTags/{id}".
     #[prost(string, tag = "1")]
     pub name: ::prost::alloc::string::String,
