4e30106dc3d28c23d729161119d8320edf8f6aab
mechiru <mechiru@users.noreply.github.com>
2024-01-09 00:39:16 +0000

    google-api-proto: regenerate code

--- google-api-proto/src/google/cloud/datacatalog/v1/mod.rs
+++ google-api-proto/src/google/cloud/datacatalog/v1/mod.rs
@@ -267,7 +267,7 @@ pub struct SystemTimestamps {
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Taxonomy {
-    /// Output only. Resource name of this taxonomy in URL format.
+    /// Identifier. Resource name of this taxonomy in URL format.
     ///
     /// Note: Policy tag manager generates unique taxonomy IDs.
     #[prost(string, tag = "1")]
@@ -373,7 +373,7 @@ pub mod taxonomy {
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct PolicyTag {
-    /// Output only. Resource name of this policy tag in the URL format.
+    /// Identifier. Resource name of this policy tag in the URL format.
     ///
     /// The policy tag manager generates unique taxonomy IDs and policy tag IDs.
     #[prost(string, tag = "1")]
--- google-api-proto/src/google/cloud/sql/v1/mod.rs
+++ google-api-proto/src/google/cloud/sql/v1/mod.rs
@@ -608,13 +608,15 @@ pub struct IpConfiguration {
     /// be updated, but it cannot be removed after it is set.
     #[prost(string, tag = "2")]
     pub private_network: ::prost::alloc::string::String,
+    /// Use `ssl_mode` instead for MySQL and PostgreSQL. SQL Server uses this flag.
+    ///
     /// Whether SSL/TLS connections over IP are enforced.
     /// If set to false, then allow both non-SSL/non-TLS and SSL/TLS connections.
     /// For SSL/TLS connections, the client certificate won't be verified. If
     /// set to true, then only allow connections encrypted with SSL/TLS and with
     /// valid client certificates. If you want to enforce SSL/TLS without enforcing
     /// the requirement for valid client certificates, then use the `ssl_mode` flag
-    /// instead of the legacy `require_ssl` flag.
+    /// instead of the `require_ssl` flag.
     #[prost(message, optional, tag = "3")]
     pub require_ssl: ::core::option::Option<bool>,
     /// The list of external networks that are allowed to connect to the instance
@@ -634,23 +636,8 @@ pub struct IpConfiguration {
     /// such as BigQuery.
     #[prost(message, optional, tag = "7")]
     pub enable_private_path_for_google_cloud_services: ::core::option::Option<bool>,
-    /// Specify how SSL/TLS is enforced in database connections. This flag is
-    /// supported only for PostgreSQL. Use the legacy `require_ssl` flag for
-    /// enforcing SSL/TLS in MySQL and SQL Server. But, for PostgreSQL, use the
-    /// `ssl_mode` flag instead of the legacy `require_ssl` flag. To avoid the
-    /// conflict between those flags in PostgreSQL, only the following value pairs
-    /// are valid:
-    ///
-    /// * `ssl_mode=ALLOW_UNENCRYPTED_AND_ENCRYPTED` and `require_ssl=false`
-    /// * `ssl_mode=ENCRYPTED_ONLY` and `require_ssl=false`
-    /// * `ssl_mode=TRUSTED_CLIENT_CERTIFICATE_REQUIRED` and `require_ssl=true`
-    ///
-    /// Note that the value of `ssl_mode` gets priority over the value of the
-    /// legacy `require_ssl`. For example, for the pair `ssl_mode=ENCRYPTED_ONLY,
-    /// require_ssl=false`, the `ssl_mode=ENCRYPTED_ONLY` means "only accepts SSL
-    /// connection", while the `require_ssl=false` means "both non-SSL and SSL
-    /// connections are allowed". The database respects `ssl_mode` in this case
-    /// and only accepts SSL connections.
+    /// SQL Server uses the `require_ssl` flag. You can set the value for this flag
+    /// to 'true' or 'false'.
     #[prost(enumeration = "ip_configuration::SslMode", tag = "8")]
     pub ssl_mode: i32,
     /// PSC settings for this instance.
@@ -1871,7 +1858,6 @@ impl SqlIpAddressType {
     }
 }
 /// The database engine type and version.
-/// The next tag is 325.
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
 #[repr(i32)]
 pub enum SqlDatabaseVersion {
@@ -4631,7 +4617,6 @@ pub struct SqlInstancesCloneRequest {
     pub body: ::core::option::Option<InstancesCloneRequest>,
 }
 /// Instance delete request.
-/// Next tag: 7
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct SqlInstancesDeleteRequest {
@@ -6072,6 +6057,11 @@ pub mod sql_external_sync_setting_error {
         /// This code instructs customers to turn on point-in-time recovery manually
         /// for the instance after promoting the Cloud SQL for PostgreSQL instance.
         TurnOnPitrAfterPromote = 36,
+        /// The minor version of replica database is incompatible with the source.
+        IncompatibleDatabaseMinorVersion = 37,
+        /// This warning message indicates that Cloud SQL uses the maximum number of
+        /// subscriptions to migrate data from the source to the destination.
+        SourceMaxSubscriptions = 38,
     }
     impl SqlExternalSyncSettingErrorType {
         /// String value of the enum field names used in the ProtoBuf definition.
@@ -6181,6 +6171,12 @@ pub mod sql_external_sync_setting_error {
                 SqlExternalSyncSettingErrorType::TurnOnPitrAfterPromote => {
                     "TURN_ON_PITR_AFTER_PROMOTE"
                 }
+                SqlExternalSyncSettingErrorType::IncompatibleDatabaseMinorVersion => {
+                    "INCOMPATIBLE_DATABASE_MINOR_VERSION"
+                }
+                SqlExternalSyncSettingErrorType::SourceMaxSubscriptions => {
+                    "SOURCE_MAX_SUBSCRIPTIONS"
+                }
             }
         }
         /// Creates an enum from field names used in the ProtoBuf definition.
@@ -6241,6 +6237,10 @@ pub mod sql_external_sync_setting_error {
                 }
                 "LOCAL_INFILE_OFF" => Some(Self::LocalInfileOff),
                 "TURN_ON_PITR_AFTER_PROMOTE" => Some(Self::TurnOnPitrAfterPromote),
+                "INCOMPATIBLE_DATABASE_MINOR_VERSION" => {
+                    Some(Self::IncompatibleDatabaseMinorVersion)
+                }
+                "SOURCE_MAX_SUBSCRIPTIONS" => Some(Self::SourceMaxSubscriptions),
                 _ => None,
             }
         }
